7a1210a611f310346ff7406df3cd278b71cf4fe1
Hisun0 <tridvad1@gmail.com>
2023-10-04 23:14:22 +0300

    add locales folder

---
+++ src/locales/en.settings.js
@@ -0,0 +1,17 @@
+export default {
+  settings: {
+    header: 'Settings',
+    content: {
+      languageHeader: 'Change language',
+      languageDropdown: 'Language',
+      themeHeader: 'Change theme',
+      themeDropdown: 'Theme',
+      dropdown: {
+        language: {
+          english: 'English',
+          russian: 'Russian',
+        },
+      },
+    },
+  },
+};
---
+++ src/locales/ru.settings.js
@@ -0,0 +1,21 @@
+export default {
+  settings: {
+    header: 'Настройки',
+    content: {
+      languageHeader: 'Сменить язык',
+      languageDropdown: 'Язык',
+      themeHeader: 'Сменить тему',
+      themeDropdown: 'Тема',
+      dropdown: {
+        language: {
+          english: 'Английский',
+          russian: 'Русский',
+        },
+        theme: {
+          dark: 'Темная',
+          light: 'Светлая',
+        },
+      },
+    },
+  },
+};
